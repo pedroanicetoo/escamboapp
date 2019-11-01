6f1516198ac39f1d95ee6db0221f2ca1e4df6ddc
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2019-11-01 01:55:13 -0300

    Criando Devises de Admin e Members e Model de Categorias por enquanto

--- escamboapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Escamboapp::Application.config.secret_token = '661021abe0386f60d1fd9911441679c22e03b09f61be33b06275dcde2aaed43f19925b824acc4008dd1cbd2a08008102bd8f957d6e4e87e0b3f5893136209ba6'
