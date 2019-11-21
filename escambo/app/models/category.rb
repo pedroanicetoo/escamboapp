b793d6b4e77c34e970602e16bf018d8489e9e333
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2019-11-21 00:43:25 -0300

    Adicionando migrate de pictures para anúncios e inserção da gem paperclip

--- escambo/app/models/category.rb
+++ escambo/app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
+
+  #scopes
+  scope :order_by_description, -> { order(:description) }
+
   attr_accessible :description
   validates_presence_of :description
 end
--- escambo/db/schema.rb
+++ escambo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20191120030400) do
+ActiveRecord::Schema.define(:version => 20191121015650) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -38,9 +38,13 @@ ActiveRecord::Schema.define(:version => 20191120030400) do
     t.text     "description"
     t.integer  "category_id"
     t.integer  "member_id"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.integer  "price_cents", :default => 0
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
+    t.integer  "price_cents",          :default => 0
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "ads", ["category_id"], :name => "index_ads_on_category_id"
